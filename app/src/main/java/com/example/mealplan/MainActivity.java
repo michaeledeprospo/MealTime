a61c25d5e12960c2a084f7b68457d0d679477149
Michael DeProspo <deprosme@dukes.jmu.edu>
2022-01-28 17:53:10 -0500

    Added logo and bg

--- app/src/main/java/com/example/mealplan/MainActivity.java
+++ app/src/main/java/com/example/mealplan/MainActivity.java
@@ -35,14 +35,7 @@ public class MainActivity extends AppCompatActivity {
         meals = new ArrayList<>();
         adapter = new SimpleAdapter(meals);
 //        fetchMeals("pasta");
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
